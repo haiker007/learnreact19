4bd71893558cb558145be3c5625eb6e1aebac1be
Zhang <yunyong.zhang@siemens.com>
2025-11-27 11:59:33 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React 19 App</h1>
+      <Button type="primary">Ant Design Button</Button>
+    </div>
+  );
+}
+
+export default App
